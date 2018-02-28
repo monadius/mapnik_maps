c126f849a0ef6748df7ce28b7d42a01e523326a2
Alexey Solovyev <solovyev.alexey@gmail.com>
2018-02-27 16:07:04 -0800

    misc

--- misc/test.py
+++ misc/test.py
@@ -1,8 +1,13 @@
+import sys
 import mapnik
-stylesheet = 'test.xml'
-image = 'test.png'
-m = mapnik.Map(600, 300)
+
+stylesheet = "test.xml"
+if len(sys.argv) > 1:
+    stylesheet = sys.argv[1]
+
+image = "test.png"
+m = mapnik.Map(800, 600)
 mapnik.load_map(m, stylesheet)
-m.zoom_all() 
+m.zoom_all()
 mapnik.render_to_file(m, image)
-print "rendered image to '%s'" % image
+print("rendered image to {}".format(image))
